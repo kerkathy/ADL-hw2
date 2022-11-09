d9a153202e321679086bd75c1f04ded7c8f79b63
kerkathy <kathyka711@gmail.com>
2022-11-09 13:15:16 +0800

    slot tag

---
+++ ADL-hw1/slot_tag_test.sh
@@ -0,0 +1,13 @@
+python slot_tagging.py \
+  --model_name_or_path bert-base-uncased \
+  --train_file data/slot/train.json \
+  --test_file data/slot/test.json \
+  --do_predict \
+  --max_seq_length 128 \
+  --label_column_name tags \
+  --output_dir result/slot/test \
+  --overwrite_output_dir \
+#   --max_train_samples 5 \
+#   --max_eval_samples 5 \
+#   --cache_dir  \
+#   remove max_{train/eval}_samples when not debugging
---
+++ ADL-hw1/slot_tag_train.sh
@@ -0,0 +1,24 @@
+python slot_tagging.py \
+  --model_name_or_path bert-base-uncased \
+  --train_file data/slot/train.json \
+  --validation_file data/slot/eval.json \
+  --do_train \
+  --do_eval \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 8 \
+  --per_device_eval_batch_size 8 \
+  --gradient_accumulation_steps 16 \
+  --eval_steps 100 \
+  --save_steps 100 \
+  --evaluation_strategy steps \
+  --learning_rate 2e-5 \
+  --num_train_epochs 8 \
+  --label_column_name tags \
+  --output_dir result/slot/train \
+  --overwrite_output_dir \
+  --load_best_model_at_end True \
+  --metric_for_best_model sentence_accuracy \
+#   --max_train_samples 5 \
+#   --max_eval_samples 5 \
+#   --cache_dir  \
+#   remove max_{train/eval}_samples when not debugging
